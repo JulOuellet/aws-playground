42f5e985d484514b8d34d29ef4ccdeaf73e2e233
JulOuellet <julien.ouellet@live.ca>
2025-02-16 15:25:54 -0500

    add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "my-test-bucket"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-execution-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "s3_write_policy" {
+  name = "S3WritePolicy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["s3:PutObject"]
+      Resource = "arn:aws:s3:::my-test-bucket/*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.s3_write_policy.arn
+}
+
+resource "aws_lambda_function" "fetch_api_data" {
+  function_name    = "hello-world"
+  role            = aws_iam_role.lambda_role.arn
+  handler         = "com.example.LambdaHandler::handleRequest"
+  runtime         = "java21"
+  filename        = "../target/aws-playground-1.0-SNAPSHOT.jar"
+  source_code_hash = filebase64sha256("../target/aws-playground-1.0-SNAPSHOT.jar")
+
+  environment {
+    variables = {
+      S3_BUCKET = aws_s3_bucket.test_bucket.bucket
+    }
+  }
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.87.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3     = "http://s3.localhost.localstack.cloud:4566"
+    lambda = "http://localhost:4566"
+    iam    = "http://localhost:4566"
+  }
+}
+
